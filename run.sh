e65224fe5a43a1042c6e14326e3c650b8ec35066
心言自由 <XinYaanZyoy@gmail.com>
2020-12-11 13:53:28 +0000

    Fri 11 Dec 2020 01:53:28 PM UTC

--- run.sh
+++ run.sh
@@ -1,4 +1,4 @@
 # install dependencies
-pip3 install jupyter pandas matplotlib # install jupyter notebook, pandas(and dependant numpy), matplotlib
+pip3 install jupyter pandas matplotlib rise==5.0.0 # install jupyter notebook, pandas(and dependant numpy), matplotlib
 jupyter-nbextension install rise --py --sys-prefix
 jupyter nbextension enable rise --py --sys-prefix
